8374f9a9364b076ac53a9cd1486feb3068d555fc
Agus Yausaz <166468372+agusyausaz@users.noreply.github.com>
2024-05-28 23:56:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -47,3 +47,28 @@ loadMoreBtn3.onclick = () => {
     }
 }
 
+function myFunction() {
+    var x = document.getElementById("My-nav-header");
+    if (x.className === "nav-header") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-header";
+    }
+  }
+
+  /*MOSTRAR MENU */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+ showMenu('nav__toggle','nav__menu')
